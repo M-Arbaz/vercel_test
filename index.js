5d201eac102512ca24ca9484df799216ae9fee8a
Muhammad Arbaz <145528265+M-Arbaz@users.noreply.github.com>
2024-08-07 12:57:46 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,9 @@
 const express = require("express");
 const app = express();
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const multer = require('multer');
-const path = require('path');
-const storage = multer.diskStorage({
-    destination: "./uploads",
-    filename: (req, file, cb) => {
-        cb(null, `file-${Date.now()}${path.extname(file.originalname)}`)
-    }
-});
-const upload = multer({ storage: storage });
-app.use(cors());
-app.use(express.json())
-app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/data', express.static('uploads'));
-app.get("/", (req, res) => res.send("Express on Vercel from Arbaz 8-7-2024 +++ gulves01 +++"));
-app.get("*",(req,res)=>{
 
-console.log("someone hit on wrong route");
+app.get("/", (req, res) => res.send("Express on Vercel from Arbaz 8-7-2024 +++ gulves +++"));
+app.get("*",(req,res)=>{
     res.send("vendoring beyound")
 });
-// app.post('/upload',upload.single("key"), async (req, res) => {
-// res.send(req.file);
-// })
 app.post("/login", (req, res) => res.send("Express on Vercel from login gulves"));
 app.listen(3000, () => console.log("Server ready on port 3000."))
