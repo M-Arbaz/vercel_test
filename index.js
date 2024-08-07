0ec5d29e24a617c5307a76680ff75d1c875d5fa7
Muhammad Arbaz <145528265+M-Arbaz@users.noreply.github.com>
2024-08-07 12:24:01 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel from Arbaz 8-7-2024 +++ gulves +++"));
+app.get("/", (req, res) => res.send("Express on Vercel from Arbaz 8-7-2024 +++ gulves0 +++"));
 app.get("*",(req,res)=>{
+
+console.log("someone hit on wrong route");
     res.send("vendoring beyound")
 });
 app.post("/login", (req, res) => res.send("Express on Vercel from login gulves"));
