8ca621338b0abf9b2a014677ab966477bba32204
Muhammad Arbaz <145528265+M-Arbaz@users.noreply.github.com>
2024-08-07 11:34:19 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel from Arbaz"));
-
+app.get("/login", (req, res) => res.send("Express on Vercel from login"));
 app.listen(3000, () => console.log("Server ready on port 3000."))
